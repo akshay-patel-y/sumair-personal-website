54d52bea1f824f64a38aeedf0124b34308a191f9
akshay-patel-y <59624240+akshay-patel-y@users.noreply.github.com>
2024-08-31 22:27:30 -0700

    save

---
+++ build/precache-manifest.cc8fc52f4cdec8b8067e9af0ce272745.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3162879d5e82b4aaa47b89d24f68746",
+    "url": "/index.html"
+  },
+  {
+    "revision": "609fcc9852535317ed77",
+    "url": "/static/css/main.6efc635e.chunk.css"
+  },
+  {
+    "revision": "386a9db861dbffc67a55",
+    "url": "/static/js/2.2bca58fb.chunk.js"
+  },
+  {
+    "revision": "62636b35e225c3fbf65aa59608134871",
+    "url": "/static/js/2.2bca58fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "609fcc9852535317ed77",
+    "url": "/static/js/main.22037d56.chunk.js"
+  },
+  {
+    "revision": "4a42fdcbdfd3cf55fea3",
+    "url": "/static/js/runtime-main.1c47a842.js"
+  },
+  {
+    "revision": "9c2f94bbb66ac585190d7fe79173224a",
+    "url": "/static/media/1910.9c2f94bb.png"
+  },
+  {
+    "revision": "7265ce28854b2fb4574caf3413b06ad0",
+    "url": "/static/media/Akshay_Patel.7265ce28.jpg"
+  },
+  {
+    "revision": "65c63d848fca0982840919fcaf7769ac",
+    "url": "/static/media/Avance.65c63d84.jfif"
+  },
+  {
+    "revision": "f8f8c91e556e2088c92d2522dff3aecc",
+    "url": "/static/media/Bonfire.f8f8c91e.png"
+  },
+  {
+    "revision": "b73c79387e77706b235f0c8c5ddc8ab6",
+    "url": "/static/media/CB.b73c7938.png"
+  },
+  {
+    "revision": "23e0292973f512c0e0ff5b03588b74f0",
+    "url": "/static/media/CommitMonoV142-400Italic.23e02929.otf"
+  },
+  {
+    "revision": "62a1171c5cc47715702aa999ed2e8914",
+    "url": "/static/media/CommitMonoV142-400Regular.62a1171c.otf"
+  },
+  {
+    "revision": "08146ef5dd5e834d35db391cb8febebd",
+    "url": "/static/media/CommitMonoV142-700Italic.08146ef5.otf"
+  },
+  {
+    "revision": "c3fbc36a8725bd3a782ea368a00c804d",
+    "url": "/static/media/CommitMonoV142-700Regular.c3fbc36a.otf"
+  },
+  {
+    "revision": "42c08b638efe0e3d56e279496c67ce13",
+    "url": "/static/media/GSV.42c08b63.png"
+  },
+  {
+    "revision": "aedfb0ad3f7109a5764c962b6c9816bc",
+    "url": "/static/media/LA.aedfb0ad.gif"
+  },
+  {
+    "revision": "948d90444d986e9734b6f9acab61cf0c",
+    "url": "/static/media/Loop.948d9044.png"
+  },
+  {
+    "revision": "5c61a52fe5014529d21bd785998f8c3b",
+    "url": "/static/media/Zighra.5c61a52f.png"
+  },
+  {
+    "revision": "e8d87581e4918c28869529ba2f767d18",
+    "url": "/static/media/blendoor.e8d87581.PNG"
+  },
+  {
+    "revision": "c2389534e7733835d2bdd8adbd037d97",
+    "url": "/static/media/bvc.c2389534.png"
+  },
+  {
+    "revision": "77929c706c815dc32973614ddacced7f",
+    "url": "/static/media/oll.77929c70.PNG"
+  }
+]);
