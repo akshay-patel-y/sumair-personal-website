ada58c7361b0d8d1b76f5a3e744cc36a67630600
akshay-patel-y <59624240+akshay-patel-y@users.noreply.github.com>
2024-10-16 21:00:40 -0700

    save

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -29,8 +29,7 @@ const Profile: React.FC<ProfileProps> = ({ name, bio, tag, title }) => {
       {/** This is my Profile Section */}
       <div className="profile-container">
         <p className="bio">{bio}</p>
-        <a href="" className="fa fa-github fa-3x"></a>
-        <a href="" className="fa fa-linkedin fa-3x"></a>
+        <a href="https://www.linkedin.com/in/sumair-ahuja-610517257/?originalSubdomain=in" className="fa fa-linkedin fa-3x"></a>
         <a href="" className="fa fa-envelope fa-3x"></a>
       </div>
     </div>
