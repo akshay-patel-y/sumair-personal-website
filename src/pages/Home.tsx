af5ca960547650687e1586413b757f1d61acfc76
akshay-patel-y <59624240+akshay-patel-y@users.noreply.github.com>
2024-10-16 20:23:27 -0700

    save

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,7 +10,7 @@ const Home: React.FC = () => {
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
       ></link>
       <Profile
-        name="akshay"
+        name="sumair"
         bio="i'm currently a software engineer at typeface ai, where i work on building an ai-powered marketing agent for enterprise teams. in addition to my technical role, i'm investing as a scout at pear vc and pantera capital, where i help identify and support promising startups in the ai and blockchain spaces. in my free time, i enjoy exploring the bay area, trying out new tech gadgets, and watching fc barcelona."
         tag="Resume"
         title="University of California, Berkeley M.E.T 22'"
