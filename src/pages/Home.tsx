d021143dc0b36cc963ac597ec3ce00841a466795
akshay-patel-y <59624240+akshay-patel-y@users.noreply.github.com>
2024-10-23 23:27:46 -0700

    save

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,7 +12,7 @@ const Home: React.FC = () => {
       ></link>
       <Profile
         name="Sumair"
-        bio="Welcome to my personal journey—a story of curiosity, action, and community-building. As a student at The Shri Ram School, I lead the student council, mentor younger students, and pursue the International Baccalaureate. Beyond academics, I founded Thrive Today to bridge socioeconomic divides by teaching soft skills to underserved students and Up SportsHub to promote sports culture through upcycling. My work extends to independent research on education equity and cognitive biases in decision-making. Whether organizing leadership programs, conducting research, or exploring STEM projects, I’m driven by a passion to empower others and create environments where everyone can thrive."
+        bio="This is my personal journey of curiosity, action, and community-building. As a student at The Shri Ram School, I lead the student council, mentor younger students, and pursue the International Baccalaureate. Beyond academics, I founded Thrive Today to bridge socioeconomic divides by teaching soft skills to underserved students and Up SportsHub to promote sports culture through upcycling. My work extends to independent research on education equity and cognitive biases in decision-making. Whether organizing leadership programs, conducting research, or exploring STEM projects, I’m driven by a passion to empower others and create environments where everyone can thrive."
         tag="Resume"
         title="University of California, Berkeley M.E.T 22'"
       />
